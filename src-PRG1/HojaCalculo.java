a57a22f7d83ee4f6e961d0eb2f498aefe102a3a2
Celia Becerril Riaño <144801825+celiabecerril@users.noreply.github.com>
2024-03-24 14:19:29 +0100

    Añade lógica de entrada por teclado
    (procesarEntrada(String input, Scanner scanner)) Es el método que se encarga de realizar todas las operaciones de movimiento o edición en la hoja de cálculo

--- src-PRG1/HojaCalculo.java
+++ src-PRG1/HojaCalculo.java
@@ -3,17 +3,14 @@ package Repos;
 import java.util.Scanner;
 
 public class HojaCalculo {
-
-
     private static final int NUM_FILAS = 16;
     private static final int NUM_COLUMNAS = 11;
     private static String[][] matriz = new String[NUM_FILAS][NUM_COLUMNAS];
     private static int filaAct = 1;
     private static int colAct = 1;
 
-
     public static void inicializarMatriz() {
-        
+
         for (int i = 1; i < NUM_FILAS; i++) {
             for (int j = 1; j < NUM_COLUMNAS; j++) {
                 matriz[i][j] = "";
@@ -30,19 +27,46 @@ public class HojaCalculo {
 
     public static void interactuarConUsuario() {
 
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Por defecto se encuentra en 'a1'. Ingrese 'w', 'a', 's', 'd' para navegar, 'e' para editar, o 'q' para salir:");
+            System.out.println(
+                    "Por defecto se encuentra en 'a1'. Ingrese 'w', 'a', 's', 'd' para navegar, 'e' para editar, o 'q' para salir:");
             String entrada = scanner.nextLine();
             if (entrada.equalsIgnoreCase("q")) {
                 break;
             }
-            
+            procesarEntrada(entrada, scanner);
             System.out.println("Ahora estás en la celda " + matriz[0][colAct] + filaAct);
             mostrarMatriz();
         }
 
         scanner.close();
+    }
+
+    public static void procesarEntrada(String input, Scanner scanner) {
+        switch (input.toLowerCase()) {
+            case "w":
+                filaAct = Math.max(1, filaAct - 1);
+                break;
+            case "a":
+                colAct = Math.max(1, colAct - 1);
+                break;
+            case "s":
+                filaAct = Math.min(NUM_FILAS - 1, filaAct + 1);
+                break;
+            case "d":
+                colAct = Math.min(NUM_COLUMNAS - 1, colAct + 1);
+                break;
+            case "e":
+                System.out.println("Ingrese el nuevo dato para la celda " + matriz[0][colAct] + filaAct + ":");
+                String nuevoValor = scanner.nextLine();
+                if (nuevoValor.length() > 7) {
+                    nuevoValor = nuevoValor.substring(0, 7);
+                }
+                matriz[filaAct][colAct] = nuevoValor;
+                break;
+        }
     }
 
     public static void mostrarMatriz() {
@@ -67,8 +91,5 @@ public class HojaCalculo {
     public static void main(String[] args) {
         inicializarMatriz();
         interactuarConUsuario();
-        mostrarMatriz();
     }
-
-    
 }
